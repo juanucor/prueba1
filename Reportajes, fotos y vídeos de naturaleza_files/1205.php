79049b8cd0547c6bdeea95827e4c016e46e55dc3
Juan Miguel Núñez Corbi <juanucor@gmail.com>
2019-11-06 20:25:53 +0100

    Primer commit

---
+++ Reportajes, fotos y vídeos de naturaleza_files/1205.php
@@ -0,0 +1,20 @@
+var site=window.location.href;
+
+
+
+  var ddx_script = document.createElement("script"); 
+ddx_script.id = "ddx_script8"; 
+ddx_script.src = 'https://dmp.citiservi.es/pxtrack.js?mode=external&acc=85';
+ddx_script.async=true;
+document.body.appendChild(ddx_script);
+
+  var _exaudiddx=_exaudiddx||[]; 	
+  _exaudiddx.push(["S",["_adv","122187"]]); 	
+  _exaudiddx.push(["S",["_tagId","10600"]]); 	
+  (function($n,e,o,d,a,t,$a) { 		
+    var x = $n.createElement(e), s = $n.getElementsByTagName(e)[0];
+    x.src = ('https:' == t.protocol ? 'https://c' : 'http://c') + o + d; 
+    x.async = a;s.parentNode.insertBefore(x, s); 	
+  }(document,"script",".neodatagroup.com/","delidatax.js",1,location,1));
+ 
+
